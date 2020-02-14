8c13c463c38bef62d22dbfbe0ed72f2f213b695b
shshchkn <shshchkn@yandex.ru>
2020-02-15 00:12:55 +0200

    App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Child from 'components/Child/Child';
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleMinus = e => {
+    if (this.state.counter > 0) {
+      this.setState(state => ({ counter: state.counter - 1 }));
+    }
+  };
+
+  handlePlus = e => {
+    this.setState(state => ({ counter: state.counter + 1 }));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <img src={logo} alt="logo" width="150" />
+            <div>{`counter: ${this.state.counter}`}</div>
+            <button className="btn btn-primary" onClick={this.handleMinus}>
+              -
+            </button>
+            <button className="btn btn-primary" onClick={this.handlePlus}>
+              +
+            </button>
+
+            <Child
+              var={this.state.counter}
+              condition={this.state.counter % 2 === 0}
+            >
+              <p>Some text</p>
+            </Child>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
